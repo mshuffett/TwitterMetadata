a0c35a98d49904f710ba2769b314df2d26fcd32d
Michael Shuffett <mshuffett@gmail.com>
2014-04-24 02:46:31 -0400

    initial commit

---
+++ example.py
@@ -0,0 +1,39 @@
+# straight from the wtforms docs:
+class TelephoneForm(Form):
+    country_code = IntegerField('Country Code', [validators.required()])
+    area_code = IntegerField('Area Code/Exchange', [validators.required()])
+    number = TextField('Number')
+
+
+class ExampleForm(Form):
+    field1 = TextField('First Field', description='This is field one.')
+    field2 = TextField('Second Field', description='This is field two.',
+                       validators=[Required()])
+    hidden_field = HiddenField('You cannot see this', description='Nope')
+    recaptcha = RecaptchaField('A sample recaptcha field')
+    radio_field = RadioField('This is a radio field', choices=[
+        ('head_radio', 'Head radio'),
+        ('radio_76fm', "Radio '76 FM"),
+        ('lips_106', 'Lips 106'),
+        ('wctr', 'WCTR'),
+        ])
+    checkbox_field = BooleanField('This is a checkbox',
+                                  description='Checkboxes can be tricky.')
+
+    # subforms
+    mobile_phone = FormField(TelephoneForm)
+
+    # you can change the label as well
+    office_phone = FormField(TelephoneForm, label='Your office phone')
+
+    submit_button = SubmitField('Submit Form')
+
+    def validate_hidden_field(form, field):
+        raise ValidationError('Always wrong')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = ExampleForm()
+    form.validate_on_submit() # to get error messages to the browser
+    return render_template('index.html', form=form)
