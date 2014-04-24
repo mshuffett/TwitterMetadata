c361ec9e84e9c5add299a98a3c707fb7b2009491
Michael Shuffett <mshuffett@gmail.com>
2014-04-24 06:32:02 -0400

    Unicode fixes and changed '\\N' handling

--- tweetid/models.py
+++ tweetid/models.py
@@ -38,7 +38,7 @@ class ModelMixin(object):
 
 
 association_table = db.Table(
-    'association', db.metadata,
+    'association',
     db.Column('collection_id', db.Integer, db.ForeignKey('collection.id')),
     db.Column('tweet_id', db.String, db.ForeignKey('tweet.id')))
 
@@ -46,21 +46,21 @@ association_table = db.Table(
 class Tweet(ModelMixin, db.Model):
     id = db.Column(db.String, primary_key=True)
     text = db.Column(db.String)
-    created_at = db.Column(db.DateTime)
+    created_at = db.Column(db.String)
     screen_name = db.Column(db.String)
     latitude = db.Column(db.String)
     longitude = db.Column(db.String)
     url_mentions = db.Column(db.String)
 
-    def __init__(self, id, text, created_at, screen_name, latitude, longitude, url_mentions=None):
-        self.id = id
-        self.text = text
-        self.created_at = created_at
-        self.screen_name = screen_name
-        self.latitude = latitude
-        self.longitude = longitude
-        if url_mentions is not None:
-            self.url_mentions = url_mentions
+    # def __init__(self, id, text, created_at, screen_name, latitude, longitude, url_mentions=None):
+    #     self.id = id
+    #     self.text = text
+    #     self.created_at = created_at
+    #     self.screen_name = screen_name
+    #     self.latitude = latitude
+    #     self.longitude = longitude
+    #     if url_mentions is not None:
+    #         self.url_mentions = url_mentions
 
     def __repr__(self):
         return "<Tweet(id='%s')" % self.id
@@ -68,4 +68,5 @@ class Tweet(ModelMixin, db.Model):
 
 class Collection(ModelMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    tweets = db.relationship('Tweet', secondary=association_table, backref='collections')
+    tweets = db.relationship('Tweet', secondary=association_table, lazy='dynamic',
+                             backref=db.backref('collections', lazy='dynamic'))
--- tweetid/tsv.py
+++ tweetid/tsv.py
@@ -1,7 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import csv
 import tarfile
 
 from models import Tweet, Collection
+from tweetid import db
+
+
+_TWEET_FIELDS = ('id', 'text', 'created_at', 'screen_name', 'latitude', 'longitude', 'url_mentions')
+
+
+def unicode_csv_reader(utf8_data, dialect=csv.excel, **kwargs):
+    csv_reader = csv.reader(utf8_data, dialect=dialect, **kwargs)
+    for row in csv_reader:
+        yield [unicode(cell, 'utf-8', 'replace') for cell in row]
 
 
 def process_tar(path, count=0):
@@ -25,4 +38,38 @@ def process_tar(path, count=0):
     collection.save()
     # print oollection.start_date, oollection.end_date
 
-process_tar('C:/Users/Michael/Downloads/twitterdata_oklahomatornado-20130520_20130530_GMT.tsv.tar.gz')
+
+def tsv_tweet_gen(path, count=0):
+    with open(path) as tsv_file:
+        tsv = unicode_csv_reader(tsv_file, delimiter='\t')
+        for i, row in enumerate(tsv):
+            if row[-1] != '\\N' or row[-2] != '\\N':
+                print row
+                raise Exception('Unexpected format.')
+            # row = row[:-2]
+
+            kwargs = {}
+            for i, field in enumerate(_TWEET_FIELDS):
+                if row[i] != '\\N':
+                    kwargs[field] = row[i]
+
+            yield Tweet(**kwargs)
+            count -= 1
+            if count == 0:
+                break
+
+
+def process_tsv(path, count=0, chunk_size=0):
+    """
+    Load the tsv at path into database. If count is positive, limit number of tweets to load to count tweets.
+    If chunk_size is positive chunk the saves by chunk_size amount.
+    """
+    collection = Collection()
+    gen = tsv_tweet_gen(path, count=count)
+    for tweet in gen:
+        tweet.collections.append(collection)
+        db.session.add(tweet)
+    db.session.commit()
+    # print oollection.start_date, oollection.end_date
+
+# process_tar('C:/Users/Michael/Downloads/twitterdata_oklahomatornado-20130520_20130530_GMT.tsv.tar.gz')
